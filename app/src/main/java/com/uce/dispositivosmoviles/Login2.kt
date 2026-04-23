ebd2104de4c3b87a9a13f799b705a13d2fba45cf
RUBEN GUANIN <rubenguanin@gmail.com>
2026-04-22 19:05:32 -0500

    Segunda autentificacion Login: uso de Binding para inflar las objetos

--- app/src/main/java/com/uce/dispositivosmoviles/Login2.kt
+++ app/src/main/java/com/uce/dispositivosmoviles/Login2.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.uce.dispositivosmoviles.databinding.ActivityLogin2Binding
 
 class Login2 : AppCompatActivity() {
 
@@ -15,20 +16,19 @@ class Login2 : AppCompatActivity() {
     lateinit var password : TextView
     lateinit var user : TextView
 
+    lateinit var binding : ActivityLogin2Binding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
 
-        var button = findViewById<Button>(R.id.btnLogin)
-        var user= findViewById<TextView>(R.id.txtUserName)
-        var password = findViewById<TextView>(R.id.txtUserPassword)
+        binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button.setOnClickListener {
-            val userText = user.text.toString()
-            val passText = password.text.toString()
+            binding.btnLogin.setOnClickListener {
 
-            if(userText == "admin" && passText == "root"){
+            if(binding.txtUserName.text.toString() == "admin" &&
+                binding.txtUserPassword.text.toString() == "root"){
                 Toast.makeText(
                     this,
                     "Redirigiendo a la siguiente pestaña...",
